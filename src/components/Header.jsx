bada624bb05111abadceab175c457e729c2bcc47
Praveen Aththanayake <aththanayakempa@gmail.com>
2023-10-05 10:44:38 +0530

    Creating components, pages, and ThemeContext

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from "react";
+import logo from "./../assets/logo.jpg";
+import { BiSearchAlt } from "react-icons/bi";
+import { BsFillMoonStarsFill, BsFillCloudSunFill } from "react-icons/bs";
+import { ThemeContext } from "../Context/ThemeContext";
+
+function Header() {
+  const { theme, setTheme } = useContext(ThemeContext);
+  useEffect(() => {
+    console.log("theme context provider", theme);
+  });
+  return (
+    <div className="flex items-center p-3">
+      <img src={logo} width={60} height={60} />
+      <div className="flex items-center w-full p-2 mx-5 rounded-full bg-slate-200">
+        <BiSearchAlt />
+        <input
+          type="text"
+          className="px-2 bg-transparent outline-none dark:bg-cyan-500"
+          placeholder="Search Games"
+        />
+      </div>
+      <div>
+        {theme == "light" ? (
+          <BsFillMoonStarsFill
+            className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+            onClick={() => setTheme("dark")}
+          />
+        ) : (
+          <BsFillCloudSunFill
+            className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer"
+            onClick={() => setTheme("light")}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
